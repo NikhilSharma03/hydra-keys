ebe80c38923ecaa6f8b40596a9470e67b4f986ba
RajGM <rajgmsocial19@gmail.com>
2022-06-23 12:46:00 +0530

    Dev: Footer complete as per review

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,12 +3,12 @@ import type { NextPage } from 'next'
 const Footer: NextPage = () => {
 
     return (
-        <footer className="footer p-20 bg-neutral text-neutral-content grid-flow-row data-theme bg-black dark:bg-white " >
+        <footer className="footer p-20 bg-neutral text-neutral-content grid-flow-row" >
             <div className="items-center grid-flow-col">
                 <div >
                     <img src="logo.png" width="80" height="78" />
                 </div>
-                <div style={{ width: "56px" }}></div>
+                <div style={{ width: "25px" }}></div>
                 <div >
                     <p>Hydra wallet</p>
                 </div>
