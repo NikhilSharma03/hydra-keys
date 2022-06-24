4e63eafcb34070b8258e5f30b98e9cc7489d1fe9
Tess99854 <stesnime@yahoo.com>
2022-06-24 11:16:49 +0100

    added the edit spl token component

--- components/EditSPLToken.tsx
+++ components/EditSPLToken.tsx
@@ -102,6 +102,7 @@ const EditSPLToken = ({ onCancel, onSuccess, hydraPubKey }: Props) => {
                 Enter SPL token public key
               </span>
             </label>
+
             <div className="w-full md:w-2/3 flex flex-col">
               <input
                 type="text"
