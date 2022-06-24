a3830335d9ce992f2b2e3ea7df1ed0d6187729f0
Tess99854 <stesnime@yahoo.com>
2022-06-24 14:37:08 +0100

    show and hide spl form dynamically

--- components/WalletDetails.tsx
+++ components/WalletDetails.tsx
@@ -141,6 +141,7 @@ const WalletDetails = ({ initialWallet, members }: WalletDetailsProps) => {
             </div>
           ) : null}
         </div>
+
         <div className={`w-full ${showUpdateSPL ? 'block' : 'hidden'}`}>
           <EditSPLToken onCancel={toggleUpdateSPL} onSuccess={updateWallet} hydraPubKey={wallet.pubKey}/>
         </div>
