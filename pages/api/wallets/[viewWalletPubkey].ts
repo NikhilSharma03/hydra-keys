ba26cd2c272622d930d270210ac93f8114e6c457
Rishabh Vasudevan <rishabhvasudevan19@gmail.com>
2022-07-01 05:40:58 +0530

    check validity of wallet on blockchain

--- pages/api/wallets/[viewWalletPubkey].ts
+++ pages/api/wallets/[viewWalletPubkey].ts
@@ -1,40 +1,100 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { Membership, PrismaClient } from '@prisma/client';
-import { MembershipModel } from '@glasseaters/hydra-sdk';
+import { Connection, clusterApiUrl, Cluster, PublicKey } from '@solana/web3.js'
 
-const prisma=new PrismaClient();
+import { useAnchorWallet, useConnection } from '@solana/wallet-adapter-react'
+import { MembershipModel, FanoutClient, Fanout } from '@glasseaters/hydra-sdk';
 
+const prisma = new PrismaClient();
+
+function validateWallet(cluster, viewWalletPubkey) {
+    const connection = new Connection(clusterApiUrl(cluster as Cluster), 'confirmed')
+    try {
+        let publickey = new PublicKey(viewWalletPubkey);
+        let a = Fanout.fromAccountAddress(connection, publickey);
+        return true;
+    }
+    catch (error: any) {
+        console.log('wallet not on blockchain');
+        return false;
+    }
+}
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
-  ) {
+) {
     console.log(req.query);
-    let { cluster, viewWalletPubkey } = req.query; 
-    if(cluster==undefined){
-        cluster="mainnet-beta";
+    let { cluster, viewWalletPubkey } = req.query;
+    if (cluster == undefined) {
+        cluster = "devnet";//can later change to mainnet-beta
     }
+
     console.log(viewWalletPubkey);
-    const wallets=await prisma.wallet.findMany();
-    const membersdb: Membership[] =await prisma.membership.findMany();
+    const wallets = await prisma.wallet.findMany();
+    const membersdb: Membership[] = await prisma.membership.findMany();
     console.log(wallets);
     console.log(membersdb);
     const result = await prisma.$queryRaw`SELECT * FROM membership WHERE walletPubkey=${viewWalletPubkey} AND cluster=${cluster}`;
     console.log("result");
     console.log(result);
+
+
+
     for (let index = 0; index < wallets.length; index++) {
         const element = wallets[index];
         console.log(element.pubkey); //I can do this since wallet addresses must be unique and there will be no two wallets with the same address
-        if(element.pubkey===viewWalletPubkey && element.cluster==cluster){
-            res.status(200).json({found:true, 
-                wallet:element,members:result});
+        if (element.pubkey === viewWalletPubkey && element.cluster == cluster) {
+            if (element.validated) {
+                res.status(200).json({
+                    found: true,
+                    wallet: element, members: result
+                });
+                return;
+            }
+            else {
+                if (validateWallet(cluster, viewWalletPubkey)) {
+                    res.status(200).json({
+                        found: true,
+                        wallet: element, members: result
+                    });
+                    const setValidated = await prisma.wallet.update({
+                        where: {
+                            cluster_pubkey: {
+                                pubkey: viewWalletPubkey,
+                                cluster: cluster,
+                            },
+                        },
+                        data: {
+                            validated: {
+                                set: true
+                            }
+                        }
+                    })
+                    element.validated = true;
+                    return;
+                }
+                else {
+                    await prisma.wallet.delete({
+                        where: {
+                            cluster_pubkey: {
+                                pubkey: viewWalletPubkey,
+                                cluster: cluster,
+                            },
+                        },
+                    })
+                    console.log('wallet deleted');
+                    res.status(200).json({ found: false });
+                    return;
 
-            return;
+                }
+            }
+
         }
-        
+
     }
-    
-    res.status(200).json({found:false});
-    
-  }
+
+    res.status(200).json({ found: false });
+
+}
