c4bf3a7e1253a075206da4c8d942f1dbd9cc4ed1
Apratim Mahata <apra487@gmail.com>
2022-07-06 20:34:19 +0530

    add distribution alert

--- components/WalletDetails.tsx
+++ components/WalletDetails.tsx
@@ -13,6 +13,8 @@ import Link from 'next/link'
 import { useAnchorWallet, useConnection } from '@solana/wallet-adapter-react'
 import { FanoutClient } from '@glasseaters/hydra-sdk'
 import { PublicKey } from '@solana/web3.js'
+import FormStateAlert, { FormState } from './FormStateAlert'
+import { useState } from 'react'
 
 type WalletDetailsProps = {
   wallet: any
@@ -20,32 +22,32 @@ type WalletDetailsProps = {
 }
 
 const WalletDetails = ({ wallet, members }: WalletDetailsProps) => {
-
+  const [formState, setFormState] = useState('idle'as FormState)
+  const [errorMsg, setErrorMsg] = useState('')
+  const [logs, setLogs] = useState([])
   const { connection } = useConnection()
   const anchorwallet = useAnchorWallet()
 
   const handleDistribute = async (memberPubkey) => {
+    setFormState("submitting");
     if (!anchorwallet) {
       return
     }
 
     try {
-
-      // Calculate fanout public key
-      const [fanoutPubkey] = await FanoutClient.fanoutKey(wallet.name)
-
+      setLogs([]);
       const fanoutSdk = new FanoutClient(connection, anchorwallet)
 
       // Generate the distribution instructions
       let distMember1 = await fanoutSdk.distributeWalletMemberInstructions({
         distributeForMint: false,
-        fanout: fanoutPubkey,
+        fanout: new PublicKey(wallet.pubkey),
         payer: anchorwallet.publicKey,
         member: new PublicKey(memberPubkey),
       })
 
-      console.log(distMember1?.instructions);
-      
+      console.log(distMember1?.instructions)
+
       // Send the distribution instructions
       const tx = await fanoutSdk.sendInstructions(
         [...distMember1?.instructions],
@@ -53,19 +55,20 @@ const WalletDetails = ({ wallet, members }: WalletDetailsProps) => {
         anchorwallet?.publicKey
       )
 
-      console.log(tx);
-      
+      console.log(tx)
+
       if (!!tx.RpcResponseAndContext.value.err) {
         const txdetails = await connection.getTransaction(
           tx.TransactionSignature
         )
+        setFormState("success")
         console.log(txdetails, tx.RpcResponseAndContext.value.err)
       }
-
     } catch (error: any) {
-      console.error(error);
+      setFormState("error");
+      setErrorMsg(JSON.stringify(error));
+      console.error(error)
     }
-
   }
 
   return (
@@ -121,12 +124,22 @@ const WalletDetails = ({ wallet, members }: WalletDetailsProps) => {
 
           <p>Total shares: {wallet.totalShares}</p>
         </div>
+        <FormStateAlert
+          state={formState}
+          submittingMsg="Distributing funds"
+          successMsg="Successfully Distributed!"
+          errorMsg={errorMsg}
+          logs={logs}
+        />
         {/*add members table here */}
         <div
           className={`card-bordered shadow-xl w-full rounded h-80 overflow-y-scroll ${styles.membersTableBg} ${styles.borderColor}`}
         >
           {members.length > 0 ? (
-            <MembersTable members={members} onHandleDistribute={handleDistribute}/>
+            <MembersTable
+              members={members}
+              onHandleDistribute={handleDistribute}
+            />
           ) : (
             <p className="text-center text-xl font-bold">
               No members please add new members
@@ -166,7 +179,7 @@ const WalletDetails = ({ wallet, members }: WalletDetailsProps) => {
         </div>
       </div>
 
-      <AddMemberModal hydraWallet = {wallet} />
+      <AddMemberModal hydraWallet={wallet} />
     </div>
   )
 }
