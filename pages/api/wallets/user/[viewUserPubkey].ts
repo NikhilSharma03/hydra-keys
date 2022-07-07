367dae64adcec3777f8eacc1215f4ec13e500759
alexandredelaitre <alex.delaitre123@gmail.com>
2022-07-07 12:47:47 +0100

    completely switched wallets/user/<pubkey> to prisma queries only

--- pages/api/wallets/user/[viewUserPubkey].ts
+++ pages/api/wallets/user/[viewUserPubkey].ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { Membership, PrismaClient,Wallet } from '@prisma/client';
+import { clusters, Membership, PrismaClient,Wallet } from '@prisma/client';
 import { MembershipModel } from '@glasseaters/hydra-sdk';
 
 const prisma=new PrismaClient();
@@ -12,36 +12,54 @@ export default async function handler(
   ) {
     let { viewUserPubkey, cluster } = req.query;
     if(cluster==undefined){
-      cluster="mainnet-beta";
+      cluster="mainnet_beta";
     }
+    
     const wallets=await prisma.wallet.findMany(
       {
         where:{
-          authority:cluster
-        }
+          authority:viewUserPubkey.toString(),
+          cluster:<keyof typeof clusters> cluster
+        },
+        
       }
     );
+
+    const walletsUserIsIn=await prisma.membership.findMany({
+      where:{
+        memberPubkey:viewUserPubkey.toString(),
+        cluster:<keyof typeof clusters> cluster
+      }
+    })
+
     const membersdb=await prisma.membership.findMany();
     console.log(viewUserPubkey);
     console.log(viewUserPubkey.length);
     console.log(wallets);
-    //console.log(await prisma.$queryRaw`SELECT * FROM wallet WHERE pubkey=( SELECT walletPubkey FROM membership WHERE membership.memberPubkey=${viewUserPubkey})`)
-    //console.log(await prisma.$queryRaw`SELECT * FROM wallet WHERE authority=${viewUserPubkey} AND cluster=${cluster}`);
-    const result: []  = await prisma.$queryRaw`SELECT * FROM wallet WHERE pubkey=( SELECT walletPubkey FROM membership WHERE membership.memberPubkey=${viewUserPubkey}) 
-    UNION SELECT * FROM wallet WHERE authority=${viewUserPubkey} AND cluster=${cluster}`;
+    console.log(walletsUserIsIn);
 
-    console.log(result)
+    let resultCleaned:Wallet[]=wallets;
 
-    let resultCleaned:[]=[];
+    for (let index = 0; index < walletsUserIsIn.length; index++) {
+      const element = walletsUserIsIn[index].walletPubkey;
+      if(wallets.find(i=>i.pubkey === element)===undefined){ //Check if duplicates do not exist
+        let toAppend=await prisma.wallet.findMany(
+          {
+            where:{
+              pubkey:element,
+              cluster:<keyof typeof clusters> cluster
+            },
+            
+          }
+        );
+        resultCleaned.push(toAppend[0]);
 
-    for (let index = 0; index < result.length; index++) {
-      const element = result[index];
-      if(element['cluster']==cluster){
-        resultCleaned.push(element);
       }
+
+      
     }
 
-    if(result.length>0){
+    if(resultCleaned.length>0){
         res.status(200).json({found:true, 
             in:resultCleaned});
             return;
