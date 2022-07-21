73ccc1f02360106f10fe8e6280d3d6a418b97308
Abdelhakeem Osama <abdelhakeem.osama@hotmail.com>
2022-07-21 15:40:32 +0200

    Use getTokenAccountBalance for SPL token balance
    - Use Connection.getTokenAccountBalance for SPL token balance to get the
    decimals along with the raw balance.
    - Convert raw token balance to real token balance accounting for decimals.

--- utils/utils.ts
+++ utils/utils.ts
@@ -25,3 +25,23 @@ export const walletHasSplToken = async (
 
   return accounts.includes(splToken)
 }
+
+// Reference: https://github.com/solana-labs/solana/blob/b05c7d91ed4e0279ec622584edb54c9ef8547ad1/account-decoder/src/parse_token.rs#L200-L220
+export const rawAmountToRealString = (rawAmount: string, decimals: number) => {
+  if (decimals === 0) {
+    return rawAmount
+  }
+
+  // Left-pad with zeros
+  let amount = rawAmount.padStart(decimals + 1, '0')
+
+  // Insert decimal point
+  const decimalPoint = amount.length - decimals
+  amount = amount.slice(0, decimalPoint) + '.' + amount.slice(decimalPoint)
+
+  // Remove trailing zeros and trailing decimal point
+  amount = amount.replace(/0+$/, '')
+  amount = amount.replace(/\.$/, '')
+
+  return amount
+}
